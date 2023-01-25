497ec27aeaa5b25d5aa92d058dff26851fc5a603
Thi Vu <thi.vu@tuni.fi>
2023-01-25 10:44:28 +0200

    problem202

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 
-
 int main()
 {
     std::cout << "How many numbers would you like to have? ";
+    int number = 0;
+    std::cin >> number;
+    int k = 1;
+    while (k <= number)
+    {
+        int remain3 = (k % 3);
+        int remain7 = (k % 7);
+        if (remain3<1) {
+            if (remain7<1){
+                std::cout <<"zip boing" << std::endl;
+            }
+            else std::cout << "zip" << std::endl;
+        } else if (remain7 < 1) {
+            std::cout << "boing" << std::endl;
+        } else std::cout << k << std::endl ;
+        k+=1;
+    }
 
-    // Write your code here
 
     return 0;
 }
