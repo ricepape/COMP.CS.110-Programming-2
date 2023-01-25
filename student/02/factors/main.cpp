50e2a89bb6f7fe043bdcb841a29fdad17763f19b
Thi Vu <thi.vu@tuni.fi>
2023-01-25 11:05:54 +0200

    chapter203

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
     cout << "Enter a positive number: ";
+    int number = 0;
+    cin >> number;
+    if (number < 1) {
+        cout <<"Only positive numbers accepted" << endl;
+    } else {
+        int factor1 = 0;
+        int factor2 = 0;
+        int root = sqrt(number);
+        int k = 1;
+        while (k <= root){
+            int remainder = number % k;
+            if (remainder < 1){
+                factor1 = k;
+                factor2 = number / k;
+            }
+            k+=1;
+        }
+        cout << number <<" = "<< factor1 <<" * "<< factor2 << endl;
 
-    // Write your code here
 
+}
     return 0;
 }
