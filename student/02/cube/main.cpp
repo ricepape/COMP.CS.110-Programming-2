e8f63d1b3c03de00e11daa88843f320b9be7e233
Thi Vu <thi.vu@tuni.fi>
2023-01-27 20:49:35 +0200

    cube1

---
+++ student/02/cube/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a number: ";
+    int number = 0;
+    cin >> number;
+
+    int triple = pow(number,3);
+    if (number>0) {
+        if (triple<number) {
+            cout << "Error! The cube of " << number << " is not " << triple << endl;
+        }
+    else if (number<0){
+        if (triple>number) {
+            cout << "Error! The cube of " << number << " is not " << triple << endl;
+    }}
+    else {cout << "The cube of " << number << " is " << triple << endl;}
+}
+    return 0;
+}
