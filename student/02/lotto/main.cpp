dd490f526c8076de00dfc7b9de16a7e439d60256
Thi Vu <thi.vu@tuni.fi>
2023-01-28 13:41:51 +0200

    lotto1

---
+++ student/02/lotto/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+unsigned long int multiply(int number)
+{
+    unsigned long int total=1;
+    int t=1;
+    while (t<=number)
+    {total=total*t;
+        t+=1;
+    }
+    return total;
+}
+
+int main()
+{
+    cout << "Enter the total number of lottery balls: ";
+    int lottery=0;
+    cin >> lottery;
+    cout << "Enter the number of drawn balls: ";
+    int drawn=0;
+    cin >> drawn;
+    if (lottery<1){
+        cout << "The number of balls must be a positive number." << endl;
+        return 0;
+    } else if (drawn >= lottery){
+        cout << "The maximum number of drawn balls is the total amount of balls." << endl;
+        return 0;
+    } else {
+        int different_combinations= 0;
+        different_combinations= multiply(lottery)/(multiply(lottery-drawn)*multiply(drawn));
+        cout <<"The probability of guessing all " << drawn <<" correct is 1/" <<different_combinations << endl;
+    }
+
+
+    return 0;
+}
