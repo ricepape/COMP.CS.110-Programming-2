8448d21151cdad5f6d58b9a711726f55241f05fc
Thi Vu <thi.vu@tuni.fi>
2023-02-03 15:46:20 +0200

    swap1

---
+++ student/03/swap/main.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <iostream>
+
+// Write your swap function here.
+
+void swap (int& first,int& second)
+{
+    int middle = first;
+    first = second;
+    second = middle;
+     }
+
+
+
+#ifndef UNIT_TESTING
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int i = 0;
+    std::cin >> i;
+
+    std::cout << "Enter another integer: ";
+    int j = 0;
+    std::cin >> j;
+
+    swap(i, j);
+    std::cout << "The integers are " << i << " and " << j << std::endl;
+
+    return EXIT_SUCCESS;
+}
+#endif
