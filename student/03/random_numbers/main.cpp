faf4e51230ba14964e8c800fad10a07119b79a56
Thi Vu <thi.vu@tuni.fi>
2023-02-03 16:44:16 +0200

    random1

--- student/03/random_numbers/main.cpp
+++ student/03/random_numbers/main.cpp
@@ -4,18 +4,30 @@
 
 using namespace std;
 
-void produce_random_numbers(unsigned int lower, unsigned int upper)
+void produce_random_numbers(unsigned int lower, unsigned int upper, unsigned int seed)
 {
-    // Implement your function here
+    string word = "c";
+    while (word!="q"){
+        default_random_engine gen(seed);
+        uniform_int_distribution<int> distr(lower, upper);
+        distr(gen); distr(gen); distr(gen); distr(gen);
+        cout << "Your drawn random number is " << distr(gen) << endl;
+        cout << "Press q to quit or any other key to continue: ";
+        cin >> word;
+        cout << endl;
+    }
+
 }
 
 int main()
 {
-    unsigned int lower_bound, upper_bound;
+    unsigned int lower_bound, upper_bound, seed_value;
     cout << "Enter a lower bound: ";
     cin >> lower_bound;
     cout << "Enter an upper bound: ";
     cin >> upper_bound;
+    cout << "Enter a seed value: ";
+    cin >> seed_value;
 
     if(lower_bound >= upper_bound)
     {
@@ -24,7 +36,7 @@ int main()
         return EXIT_FAILURE;
     }
 
-    produce_random_numbers(lower_bound, upper_bound);
+    produce_random_numbers(lower_bound, upper_bound,seed_value);
 
     return EXIT_SUCCESS;
 }
