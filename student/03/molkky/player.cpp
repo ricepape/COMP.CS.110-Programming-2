354403d2f48296284e6b2458e95b6c060f919472
Thi Vu <thi.vu@tuni.fi>
2023-02-06 21:43:45 +0200

    molkky1

---
+++ student/03/molkky/player.cpp
@@ -0,0 +1,29 @@
+#include "player.hh"
+#include <iostream>
+
+Player::Player(const std::string name): name_(name)
+{
+}
+
+std::string Player::get_name(){
+    return name_;
+}
+
+int Player::get_points(){
+    return points_;
+}
+
+bool Player::has_won(){
+    if (points_==50){
+        return true;
+    }
+    else return false;
+}
+
+void Player::add_points(int pts){
+    points_+=pts;
+    if (points_>50){
+        points_=25;
+        std::cout<<name_<<" gets penalty points!"<<std::endl;
+    }
+}
---
+++ student/03/molkky/player.hh
@@ -0,0 +1,25 @@
+#ifndef PLAYER_HH
+#define PLAYER_HH
+
+# include <string>
+
+class Player
+{
+public:
+    //Constructor
+    Player(const std::string name);
+    void add_points(int pts);
+    bool has_won();
+    std::string get_name();
+    int get_points();
+
+private:
+    std::string name_;
+    int points_;
+
+
+};
+
+
+
+#endif // PLAYER_HH
