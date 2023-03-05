3758f6682cf2dda090d823e687b46ab9fef1a34a
Thi Vu <thi.vu@tuni.fi>
2023-03-05 22:58:48 +0200

    network1

--- student/06/network/main.cpp
+++ student/06/network/main.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 const std::string HELP_TEXT = "S = store id1 i2\nP = print id\n"
                               "C = count id\nD = depth id\n";
-
+using NET = std::unordered_map<std::string, std::vector<std::string>>;
+//using PERSON = std::pair<std::string,std::vector<std::string>>;
 
 std::vector<std::string> split(const std::string& s,
                                const char delimiter,
@@ -29,10 +31,63 @@ std::vector<std::string> split(const std::string& s,
     }
     return result;
 }
+void store(std::string const &id1,std::string const &id2,NET &network)
+{
+    //checking the person is in the network
+    if(network.find(id1)==network.end())
+    {
+        network.insert({id1,{}});
+    }
+    if(network.find(id2)==network.end())
+    {
+        network.insert({id2,{}});
+    }
+    network.at(id1).push_back(id2);
+}
+void print(std::string const &id,std::string const dots,NET const &network)
+{
+    std::vector<std::string> followers = network.at(id);
+
+
+    std::cout <<dots <<id<<std::endl;
+    if(followers.empty())
+    {
+        return;
+    }
+    for(std::string const &i :followers)
+    {
+        print(i,dots+"..",network);
+    }
+}
+int count(std::string const &id,NET const &network)
+{
+    std::vector<std::string> followers = network.at(id);
+    int n=0;
+    for(std::string const &i :followers)
+    {
+        n++;
+        n+= count(i,network);
+    }
+    return n;
+}
+int depth(std::string const &id,NET const &network)
+{
+    std::vector<std::string> followers = network.at(id);
+    int n=0;
+    for(std::string const &i :followers)
+    {
+        int d = depth(i,network);
+        if (d>n)
+            n=d;
+    }
+    return n+1;
+}
+
 
 int main()
 {
     // TODO: Implement the datastructure here
+    NET network;
 
 
     while(true)
