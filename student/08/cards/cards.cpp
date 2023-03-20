952ad9422b5f3e9da5daeb07a15a4ce450bcd508
Thi Vu <thi.vu@tuni.fi>
2023-03-20 12:39:13 +0200

    card4

--- student/08/cards/cards.cpp
+++ student/08/cards/cards.cpp
@@ -1,5 +1,6 @@
 #include "cards.hh"
 #include <iostream>
+#include <stack>
 
 // TODO: Implement the methods here
 Cards::Cards(): top_(nullptr) {
@@ -18,23 +19,82 @@ void Cards::print_from_top_to_bottom(std::ostream& s){
 }
 
 void Cards::print_from_bottom_to_top(std::ostream& s){
-    if (top_!=nullptr){
-       Card_data* item_to_be_printed = top_;
-       int running_number = 1;
+    if (top_ != nullptr){
+            std::stack<Card_data*> card_stack;
+            Card_data* item_to_be_printed = top_;
 
-       while ( item_to_be_printed != nullptr ) {
-          s << running_number << ". " << item_to_be_printed->data << std::endl;
-          ++running_number;
-          item_to_be_printed = item_to_be_printed->next;
-   }}
-}
+            while (item_to_be_printed != nullptr) {
+                card_stack.push(item_to_be_printed);
+                item_to_be_printed = item_to_be_printed->next;
+            }
+
+            int running_number = 1;
+            while (!card_stack.empty()) {
+                item_to_be_printed = card_stack.top();
+                s << running_number << ". " << item_to_be_printed->data << std::endl;
+                ++running_number;
+                card_stack.pop();
+            }
+        }
+    }
 
 bool Cards::bottom_to_top(){
-   return true;
+    if (top_ != nullptr){
+        std::stack<Card_data*> card_stack;
+        Card_data* item_to_be_printed = top_;
+        card_stack.push(item_to_be_printed);
+
+        while (item_to_be_printed->next != nullptr) {
+            item_to_be_printed = item_to_be_printed->next;
+            card_stack.push(item_to_be_printed);
+        }
+
+
+        item_to_be_printed = card_stack.top();
+        card_stack.pop();
+
+
+        while (!card_stack.empty()) {
+            Card_data* item = card_stack.top();
+            card_stack.pop();
+            item->next = item_to_be_printed->next;
+            item_to_be_printed->next = item;
+            top_=item_to_be_printed;
+        }
+        return true;
+
+    }
+
+   else return false;
 }
 
 bool Cards::top_to_bottom(){
-   return true;
+    if (top_ != nullptr) {
+        std::stack<Card_data*> card_stack;
+        Card_data* item_to_be_printed = top_;
+
+        while (item_to_be_printed != nullptr) {
+            card_stack.push(item_to_be_printed);
+            item_to_be_printed = item_to_be_printed->next;
+        }
+
+        item_to_be_printed = card_stack.top();
+        card_stack.pop();
+        top_ = item_to_be_printed;
+
+        while (!card_stack.empty()) {
+            Card_data* item = card_stack.top();
+            card_stack.pop();
+            item_to_be_printed->next = item;
+            item_to_be_printed = item;
+        }
+
+        item_to_be_printed->next = nullptr;
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 void Cards::add(int id){
