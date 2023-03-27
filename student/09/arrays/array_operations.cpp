4ddabc12f67d1d0df2a48ddfea1544cb4f7eeb62
Thi Vu <thi.vu@tuni.fi>
2023-03-27 09:40:28 +0300

    Array3

--- student/09/arrays/array_operations.cpp
+++ student/09/arrays/array_operations.cpp
@@ -12,12 +12,15 @@ int greatest_v1(int* itemptr, int size){
 
 int greatest_v2(int *itemptr, int *endptr)
 {
-    int max = 0;
+    int max = -10000000;
     while (itemptr < endptr){
-        if (*itemptr >= max & *itemptr!=0){
+        if (*itemptr >= max && *itemptr!=0){
             max=*itemptr;
         }
         ++itemptr;
+    }
+    if (max==-1000000){
+        max=0;
     }
     return max;
 }
