a0af67e9e2fe7b63bcd0fec6c2d10c53d2a32a91
Thi Vu <thi.vu@tuni.fi>
2023-04-07 15:27:19 +0300

    university-1st test-sign up function

---
+++ student/10/university2/course.cpp
@@ -0,0 +1,95 @@
+#include "course.hh"
+#include <iostream>
+#include <memory>
+
+Course::Course(const std::string& code, const std::string& name, int credits):
+    course_code_(code), name_(name), credits_(credits)
+{
+}
+
+Course::~Course()
+{
+}
+
+void Course::print_info(bool print_new_line)
+{
+    std::cout << course_code_ << " : " << name_;
+    if ( print_new_line )
+    {
+        std::cout << std::endl;
+    }
+}
+
+void Course::print_long_info()
+{
+    std::cout << "* Course name: " << name_ << std::endl
+              << "* Code: " << course_code_ << std::endl;
+    print_staff();
+}
+
+void Course::print_staff()
+{
+    std::cout << "* Staff: " << std::endl;
+    for (auto member : course_staff_ )
+    {
+        member->print();
+    }
+}
+
+void Course::add_staff(Account *new_staff_member)
+{
+    // Checking if account is already a staff member
+    for ( unsigned int i = 0; i < course_staff_.size(); ++i )
+    {
+        if ( course_staff_.at(i) == new_staff_member )
+        {
+            std::cout << STAFF_EXISTS << std::endl;
+            return;
+        }
+    }
+
+    course_staff_.push_back(new_staff_member);
+    std::cout << STAFF_ADDED << std::endl;
+}
+
+const std::string Course::get_code() const
+{
+    return course_code_;
+}
+
+int Course::get_credits() const
+{
+    return credits_;
+}
+
+void Course::add_student(const int &student_to_be_added) {
+   List_students* new_item = new List_students{student_to_be_added, nullptr};
+
+   if ( first_ == nullptr ) {
+      first_ = new_item;
+      last_ = new_item;
+   } else {
+      last_->next = new_item;
+      last_ = new_item;
+   }
+}
+
+bool Course::is_student_exists(const int &student_to_be_checked)
+{
+    List_students* student_lists= first_;
+    while ( student_lists != nullptr ) {
+      if (student_lists->students_signed_up != student_to_be_checked){
+          student_lists = student_lists->next;
+      }
+      else {
+          return true;
+      }
+    }
+    return false;
+ }
+
+
+
+
+
+
---
+++ student/10/university2/university.cpp
@@ -0,0 +1,203 @@
+#include "university.hh"
+#include <iostream>
+
+University::University(const std::string& email_suffix):
+    running_number_(111111), email_suffix_(email_suffix)
+{
+}
+
+University::~University()
+{
+    for ( auto course : courses_ )
+    {
+        delete course.second;
+    }
+
+    for ( auto account : accounts_ )
+    {
+        delete account.second;
+    }
+}
+
+void University::new_course(Params params)
+{
+    std::string code = params.at(0);
+    std::string name = params.at(1);
+    if ( courses_.find(code) != courses_.end() )
+    {
+        std::cout << ALREADY_EXISTS << std::endl;
+        return;
+    }
+    else
+    {
+        std::cout << NEW_COURSE << std::endl;
+    }
+
+    Course* n_course = new Course(code, name);
+    courses_.insert({n_course->get_code(), n_course});
+}
+
+void University::print_courses(Params)
+{
+    for ( auto course : courses_ )
+    {
+        course.second->print_info(true);
+    }
+}
+
+void University::print_course(Params params)
+{
+    std::string code = params.at(0);
+    if ( courses_.find(code) == courses_.end() )
+    {
+        std::cout << CANT_FIND << code << std::endl;
+        return;
+    }
+
+    courses_.at(code)->print_long_info();
+}
+
+void University::new_account(Params params)
+{
+    std::string full_name = params.at(0);
+    int num = 0;
+    for ( std::map<int, Account*>::iterator iter = accounts_.begin();
+          iter != accounts_.end();
+          ++iter )
+    {
+        if ( iter->second->get_full_name() == full_name )
+        {
+            std::string email = iter->second->get_email();
+            num = Utils::numeric_part(email);
+            if(num == 0)
+            {
+                ++num;
+            }
+            ++num;
+        }
+    }
+
+    int account_number = running_number_++;
+    Account* n_account = new Account(full_name, account_number, num,
+                                     email_suffix_);
+    accounts_.insert({account_number, n_account});
+    std::cout << NEW_ACCOUNT << std::endl;
+    n_account->print();
+}
+
+void University::print_accounts(Params)
+{
+    for ( auto account : accounts_ )
+    {
+        account.second->print();
+    }
+}
+
+void University::print_account(Params params)
+{
+    int account = std::stoi(params.at(0));
+    std::map<int, Account*>::iterator iter = accounts_.find(account);
+    if ( iter == accounts_.end() )
+    {
+        std::cout << CANT_FIND << account << std::endl;
+        return;
+    }
+    iter->second->print();
+}
+
+void University::add_staff(Params params)
+{
+    std::string code = params.at(0);
+    int account = std::stoi(params.at(1));
+    if ( courses_.find(code) == courses_.end() )
+    {
+        std::cout << CANT_FIND << code << std::endl;
+        return;
+    }
+    if ( accounts_.find(account) == accounts_.end() )
+    {
+        std::cout << CANT_FIND << account << std::endl;
+        return;
+    }
+
+    courses_.at(code)->add_staff(accounts_.at(account));
+}
+
+void University::sign_up(Params params)
+{
+    std::string code = params.at(0);
+    int account = std::stoi(params.at(1));
+    if ( courses_.find(code) == courses_.end() )
+    {
+        std::cout << CANT_FIND << code << std::endl;
+        return;
+    }
+    if ( accounts_.find(account) == accounts_.end() )
+    {
+        std::cout << CANT_FIND << account << std::endl;
+        return;
+    }
+    Course* chosen_course =  courses_.at(code);
+    Account* chosen_account = accounts_.at(account);
+
+    if (chosen_course->is_student_exists(account)){
+            std::cout << STUDENT_EXISTS << std::endl;
+            return;
+        }
+
+    std::vector<std::string> list_courses = chosen_account->get_completed_courses();
+    if (chosen_account->get_graduated() == true){
+        std::cout << ALREADY_GRADUATED << std::endl;
+        return;
+    }
+    for (auto & element: list_courses){
+        if (element == code){
+            std::cout << STUDENT_COMPLETED << std::endl;
+            return;
+        }
+    }
+    chosen_course->add_student(account);
+    std::cout << SIGNED_UP << std::endl;
+}
+
+void University::complete(Params params)
+{
+    std::string code = params.at(0);
+    int account = std::stoi(params.at(1));
+    if ( courses_.find(code) == courses_.end() )
+    {
+        std::cout << CANT_FIND << code << std::endl;
+        return;
+    }
+    if ( accounts_.find(account) == accounts_.end() )
+    {
+        std::cout << CANT_FIND << account << std::endl;
+        return;
+    }
+    Course* chosen_course =  courses_[code];
+    Account* chosen_account = accounts_[account];
+
+
+
+}
+
+
+void University::print_signups(Params params)
+{
+
+}
+
+void University::print_completed(Params params)
+{
+
+}
+
+void University::print_study_state(Params params)
+{
+
+}
+
+void University::graduate(Params params)
+{
+
+}
