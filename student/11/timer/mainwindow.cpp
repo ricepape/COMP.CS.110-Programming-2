f2a66a33e3621f1fcb6f5aeb33b9d8ab9ea8fe4b
Thi Vu <thi.vu@tuni.fi>
2023-04-17 18:20:41 +0300

    timer1

--- student/11/timer/mainwindow.cpp
+++ student/11/timer/mainwindow.cpp
@@ -6,9 +6,45 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->lcdNumberMin->setStyleSheet("background-color:green;");
+    ui->lcdNumberSec->setStyleSheet("background-color:dark_green;");
+
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(on_lcdNumberSec_overflow()));
+    timer->start(1000);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_startButton_clicked()
+{
+    timer->start(1000);
+}
+
+void MainWindow::on_resetButton_clicked()
+{
+    ui->lcdNumberMin->display(0);
+    ui->lcdNumberSec->display(0);
+}
+
+void MainWindow::on_stopButton_clicked()
+{
+    timer->stop();
+}
+
+void MainWindow::on_lcdNumberSec_overflow()
+{
+    int sec = ui->lcdNumberSec->intValue();
+    int min = ui->lcdNumberMin->intValue();
+
+    sec++;
+    if (sec == 60) {
+        sec = 0;
+        min++;
+        ui->lcdNumberMin->display(min);
+    }
+    ui->lcdNumberSec->display(sec);
 }
--- student/11/timer/mainwindow.hh
+++ student/11/timer/mainwindow.hh
@@ -24,6 +24,14 @@ public:
 private slots:
     // Add your slots here
 
+    void on_startButton_clicked();
+
+    void on_resetButton_clicked();
+
+    void on_stopButton_clicked();
+
+    void on_lcdNumberSec_overflow();
+
 private:
     Ui::MainWindow *ui;
     // Add other necessary members here
